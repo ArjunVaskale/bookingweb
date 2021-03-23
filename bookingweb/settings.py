2f2dd7fb2c60e73259739fdea25931118db58352
ArjunVaskale <36169177+ArjunVaskale@users.noreply.github.com>
2021-03-24 00:19:59 +0530

    Update settings.py

--- bookingweb/settings.py
+++ bookingweb/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'k(aj8+7e^u88v)6g&)h0qw*q7#_iu^e+0b+x=4y&%0ubt1s+wq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True    
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',            'bookingweb.herokuapp.com']
 
 # Application definition
 
